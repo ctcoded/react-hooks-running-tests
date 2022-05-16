a3fbe9cecf15f52f50ac08e2fbe9e887003b889a
cesartidrick <cesartidrick@gmail.com>
2022-05-16 14:53:07 -0600

    commit tree be21e98aceacedf6c567edc56602a09678c9fbf2

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,11 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
 
 export default Article;
+
+// screen.debug();
